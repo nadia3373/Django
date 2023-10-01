6b91b754ff74593b21a97653760f9a6aa06a0e87
nadia3373 <3773373@mail.ru>
2023-10-01 14:43:43 +0700

    added hw2&3

---
+++ s4/store/views.py
@@ -0,0 +1,140 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .models import Client, Product, Order
+from .forms import ClientForm, ProductForm, OrderForm
+
+
+def index(request):
+    return render(request, 'base.html')
+
+
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'store/client_list.html', {'clients': clients})
+
+
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    return render(request, 'store/client_detail.html', {'client': client})
+
+
+def client_create(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm()
+    return render(request, 'store/client_form.html', {'form': form})
+
+
+def client_edit(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            client = form.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'store/client_form.html', {'form': form})
+
+
+def client_delete(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client_list')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ProductForm()
+    return render(request, 'store/product_form.html', {'form': form})
+
+
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'store/product_form.html', {'form': form})
+
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+
+
+def order_list(request):
+    orders = Order.objects.all()
+    today = timezone.now()
+    return render(request, 'store/order_list.html', {
+        'orders_7_days': orders.filter(
+            order_date__gte=today - timezone.timedelta(days=7), order_date__lte=today
+        ),
+        'orders_30_days': orders.filter(
+            order_date__gte=today - timezone.timedelta(days=30), order_date__lte=today
+        ),
+        'orders_365_days': orders.filter(
+            order_date__gte=today - timezone.timedelta(days=365), order_date__lte=today
+        ),
+    })
+
+
+def order_detail(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    return render(request, 'store/order_detail.html', {'order': order})
+
+
+def order_create(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            return redirect('order_detail', pk=order.pk)
+    else:
+        form = OrderForm()
+    return render(request, 'store/order_form.html', {'form': form})
+
+
+def order_edit(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            order = form.save()
+            return redirect('order_detail', pk=order.pk)
+    else:
+        form = OrderForm(instance=order)
+    return render(request, 'store/order_form.html', {'form': form})
+
+
+def order_delete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('order_list')
